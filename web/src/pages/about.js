45336b75d02b823996aa6ac31b3e4de52cb2b2be
Cameron <monaghancj@g.cofc.edu>
2016-11-22 14:36:03 -0500

    Step-2 - Created Home and About pages

---
+++ web/src/pages/about.js
@@ -0,0 +1,11 @@
+const React = require('react')
+
+const About = React.createClass({
+  render() {
+    return (
+      <div>About Relief Tracker</div>
+    )
+  }
+})
+
+module.exports = About
---
+++ web/src/pages/home.js
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const Home = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>おかえりなさい</h1>
+      </div>
+    )
+  }
+})
+
+module.exports = Home
